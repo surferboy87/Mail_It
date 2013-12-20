72b1fbf058562cb6d22f4118f578192b5c022a43
surferboy87 <surferboy87@github.com>
2013-12-20 21:59:01 +0100

    Deleted logging test in case there cann't be two main Methods. Additional added a Label in GUI that something will be presented

--- Mail_It/src/Mail_It/Main.java
+++ Mail_It/src/Mail_It/Main.java
@@ -1,10 +1,10 @@
 package Mail_It;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
