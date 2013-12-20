3874eb1dae55ef45b1fca89519bcae6a5b021c91
surferboy87 <surferboy87@github.com>
2013-12-20 19:01:51 +0100

    Changed apperance in testLog

--- Mail_It/src/Mail_It/testLog.java
+++ Mail_It/src/Mail_It/testLog.java
@@ -12,11 +12,14 @@ public class testLog {
 	
 	/* Get actual class name to be printed on */
 	
-	
-	public static void main(String[] args) {
 		private static org.apache.log4j.Logger log = Logger.getLogger(LogClass.class);
-		public static void main(String[] args) { log.trace("Trace Message!"); log.debug("Debug Message!"); log.info("Info Message!"); log.warn("Warn Message!"); log.error("Error Message!"); log.fatal("Fatal Message!");
+		
+		public static void main(String[] args) {
+		log.trace("Trace Message!");
+		log.debug("Debug Message!");
+		log.info("Info Message!");
+		log.warn("Warn Message!");
+		log.error("Error Message!");
+		log.fatal("Fatal Message!");
 		}
 	}
-
-}
