48082ca6085718483fd7d625cd11fcea1314b942
Sebastian Nellen <sebastian@nellen.it>
2013-12-20 19:06:29 +0100

    fixed testLog

--- Mail_It/src/Mail_It/testLog.java
+++ Mail_It/src/Mail_It/testLog.java
@@ -9,17 +9,17 @@ import java.util.*;
 
 public class testLog {
 
-	
+
 	/* Get actual class name to be printed on */
-	
-		private static org.apache.log4j.Logger log = Logger.getLogger(LogClass.class);
-		
-		public static void main(String[] args) {
+
+	private static Logger log = Logger.getLogger(testLog.class.getName());
+
+	public static void main(String[] args) {
 		log.trace("Trace Message!");
 		log.debug("Debug Message!");
 		log.info("Info Message!");
 		log.warn("Warn Message!");
 		log.error("Error Message!");
 		log.fatal("Fatal Message!");
-		}
 	}
+}
